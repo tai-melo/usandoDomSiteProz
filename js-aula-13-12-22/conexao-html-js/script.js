ebedb561aae3656426664938bcc5636fbbe90181
Tairine de Melo <tairine.me@gmail.com>
2022-12-14 21:53:18 -0300

    inserindo outros valores atraves do DOM

---
+++ js-aula-13-12-22/conexao-html-js/script.js
@@ -0,0 +1,52 @@
+//alteração do autor do post 01
+
+const nome_tai = document.getElementById("post01");
+
+nome_tai.children[1].innerHTML = "<strong> Autor: </strong> Tairine";
+
+console.log(nome_tai.children[1].innerHTML);
+
+//alteração do título do post 02
+
+const titulo_comentario = document.getElementById("subtitulo_comentario");
+
+titulo_comentario.innerText = "Conteúdos instigantes";
+
+//alteranção nome do Blog
+
+let nome_blog = document.getElementById("nome-blog-principal");
+
+console.log(nome_blog);
+
+nome_blog.innerText = "Blog dos Alunos"
+
+
+
+
+
+
+
+
+
+
+//alteração do conteúdo do post 02 (ainda em andamento)
+
+//const conteudo_post = document.getElementsByClassName("child")[0].innerHTML = "Teste";
+
+//console.log(conteudo_post)
+
+//const conteudo_post = document.getElementById("post-texto-");
+
+
+// ESSE AQUI FOI A MONICA QUE ME MANDOU :const conteudo_post = document.getElementsByClassName("post-texto-02")[0];
+// conteudo_post.getElementsByClassName("post-texto-02")[0].innerHTML = "Teste"; --> Testei e tbm não funcionou
+
+
+
+
+
+
+
+
+
+
